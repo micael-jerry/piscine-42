3cc24051708badb0405ec49971f6ff9290d3f706
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-13 20:48:14 +0300

    chore pc5 - ex03

---
+++ piscine-c-05/ex03/ft_recursive_power.c
@@ -0,0 +1,28 @@
+int ft_recursive_power(int nb, int power)
+{
+    if (power < 0)
+    {
+        return (0);
+    }
+    else if (power == 0)
+    {
+        return (1);
+    }
+    else if (power == 1)
+    {
+        return (nb);
+    }
+    return (nb * ft_recursive_power(nb, power - 1));
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    int nb = -5;
+    int power = 4;
+    int res = ft_recursive_power(nb, power);
+    printf("%d", res);
+    return 0;
+}
+*/
