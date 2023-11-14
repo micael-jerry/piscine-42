9b35459124342932c79ed2e0f67b2d43e090e50b
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-14 21:14:18 +0300

    fix: pc6  ex01 ex03

---
+++ piscine-c-06/ex03/ft_sort_params.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while ((s1[i] == s2[i]) && s1[i] != '\0' && s2[i] != '\0')
+    {
+        i++;
+    }
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+}
+
+void ft_print_params(int argc, char *argv[])
+{
+    int i;
+
+    i = 1;
+    while (i < argc)
+    {
+        ft_putstr(argv[i]);
+        ft_putchar('\n');
+        i++;
+    }
+}
+
+void ft_sort_params(int argc, char *argv[])
+{
+    int i;
+    int j;
+    char *temp;
+
+    i = 1;
+    while (i < argc)
+    {
+        j = i + 1;
+        while (j < argc)
+        {
+            if (ft_strcmp(argv[i], argv[j]) < 0)
+            {
+                temp = argv[i];
+                argv[i] = argv[j];
+                argv[j] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+    
+}
+
+int main(int argc, char *argv[])
+{
+    ft_sort_params(argc, argv);
+    ft_print_params(argc, argv);
+    return (0);
+}
