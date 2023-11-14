f6338501934dfb2748bb28a17556a7c9ab0ef536
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-14 05:47:49 +0300

    chore pc5 - ex05

---
+++ piscine-c-05/ex05/ft_sqrt.c
@@ -0,0 +1,28 @@
+int find_sqrt(int nb, int i)
+{
+    if ((nb < 0) || ((i * i) > nb))
+    {
+        return (0);
+    }
+    else if ((i * i) == nb)
+    {
+        return (i);
+    }
+    return (find_sqrt(nb, i + 1));
+}
+
+int ft_sqrt(int nb)
+{
+    int res = find_sqrt(nb, 0);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    int nb = 4;
+    int res = ft_sqrt(nb);
+    printf("%d", res);
+    return 0;
+}
+*/
