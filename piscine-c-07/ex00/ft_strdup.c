14ae36b6e874de2d5f24087203547c0d3df60513
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-15 18:11:25 +0300

    chore pc7 - ex00

---
+++ piscine-c-07/ex00/ft_strdup.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
+
+char *ft_strdup(char *src)
+{
+    int i;
+    char *copy;
+    
+    i = 0;
+    copy = malloc(sizeof(char) * ft_strlen(src));
+    while (src[i] != '\0')
+    {
+        copy[i] = src[i];
+        i++;
+    }
+    copy[i] = '\0';
+    return (copy);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+    char *str = "test";
+    char *cp = ft_strdup(str);
+    printf("%s\n", cp);
+    return 0;
+}
+*/
