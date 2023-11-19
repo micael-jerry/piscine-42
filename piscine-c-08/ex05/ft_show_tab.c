260eec909f1872ebce7d2f6e63fc8dfecc445c82
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-19 19:15:36 +0300

    pc8 ex05

--- piscine-c-08/ex05/ft_show_tab.c
+++ piscine-c-08/ex05/ft_show_tab.c
@@ -1,79 +1,79 @@
-#include <unistd.h>
 #include "ft_stock_str.h"
+#include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    unsigned int n;
+	unsigned int	n;
 
-    if (nb < 0)
-    {
-        ft_putchar('-');
-        n = -nb;
-    }
-    else
-    {
-        n = nb;
-    }
-    if (n > 9)
-    {
-        ft_putnbr(n / 10);
-        n = n % 10;
-    }
-    ft_putchar(n + '0');
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		n = -nb;
+	}
+	else
+	{
+		n = nb;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		n = n % 10;
+	}
+	ft_putchar(n + '0');
 }
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        ft_putchar(str[i]);
-        i++;
-    }
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
 }
 
-void ft_show_stock_str(t_stock_str stock)
+void	ft_show_stock_str(t_stock_str stock)
 {
-    ft_putstr(stock.str);
-    ft_putchar('\n');
-    ft_putnbr(stock.size);
-    ft_putchar('\n');
-    ft_putstr(stock.copy);
-    ft_putchar('\n');
+	ft_putstr(stock.str);
+	ft_putchar('\n');
+	ft_putnbr(stock.size);
+	ft_putchar('\n');
+	ft_putstr(stock.copy);
+	ft_putchar('\n');
 }
 
-void ft_show_tab(struct s_stock_str *par)
+void	ft_show_tab(struct s_stock_str *par)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (par[i].str != 0)
-    {
-        ft_show_stock_str(par[i]);
-        i++;
-    }
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_show_stock_str(par[i]);
+		i++;
+	}
 }
 
-int main()
+int	main(void)
 {
-    char str_1[] = "test1";
-    char str_2[] = "test2";
-    char str_3[] = "test3";
-    char *strs[3];
+	char str_1[] = "test1";
+	char str_2[] = "test2";
+	char str_3[] = "test3";
+	char *strs[3];
 
-    strs[0] = str_1;
-    strs[1] = str_2;
-    strs[2] = str_3;
+	strs[0] = str_1;
+	strs[1] = str_2;
+	strs[2] = str_3;
 
-    t_stock_str *res = ft_strs_to_tab(3, strs);
-    ft_show_tab(res);
-    free(res);
-    return (0);
+	t_stock_str *res = ft_strs_to_tab(3, strs);
+	ft_show_tab(res);
+	free(res);
+	return (0);
 }
