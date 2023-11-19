0a9ddbf4f5d564a206309932c57ad8ea47a2a6a2
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-19 09:07:49 +0300

    pc8 ex05

---
+++ piscine-c-08/ex05/ft_show_tab.c
@@ -0,0 +1,141 @@
+#include <stdlib.h>
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+    unsigned int n;
+
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        n = -nb;
+    }
+    else
+    {
+        n = nb;
+    }
+    if (n > 9)
+    {
+        ft_putnbr(n / 10);
+        n = n % 10;
+    }
+    ft_putchar(n + '0');
+}
+
+void ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+}
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
+
+char *ft_strdup(char *src)
+{
+    int i;
+    char
+        *copy;
+
+    i = 0;
+    copy = malloc(sizeof(char) * ft_strlen(src) + sizeof('\0'));
+    if (!(copy))
+        return (NULL);
+    while (src[i] != '\0')
+    {
+        copy[i] = src[i];
+        i++;
+    }
+    copy[i] = '\0';
+    return (copy);
+}
+
+t_stock_str create_stock_str(int size, char *str, char *copy)
+{
+    t_stock_str temp;
+
+    temp.size = size;
+    temp.str = str;
+    temp.copy = copy;
+
+    return (temp);
+}
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+    int i;
+    t_stock_str *result;
+
+    result = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+    if (result == NULL)
+        return (NULL);
+    i = 0;
+    while (i < ac)
+    {
+        result[i] = create_stock_str(
+            ft_strlen(av[i]), av[i], ft_strdup(av[i]));
+        i++;
+    }
+    result[i] = create_stock_str(0, "\0", "\0");
+    return (result);
+}
+
+void ft_show_stock_str(t_stock_str stock)
+{
+    ft_putstr(stock.str);
+    ft_putchar('\n');
+    ft_putnbr(stock.size);
+    ft_putchar('\n');
+    ft_putstr(stock.copy);
+    ft_putchar('\n');
+}
+
+void ft_show_tab(struct s_stock_str *par)
+{
+    int i;
+
+    i = 0;
+    while (par[i].size != 0)
+    {
+        ft_show_stock_str(par[i]);
+        i++;
+    }
+}
+
+int main()
+{
+    char str_1[] = "test1";
+    char str_2[] = "test2";
+    char str_3[] = "test3";
+    char *strs[3];
+
+    strs[0] = str_1;
+    strs[1] = str_2;
+    strs[2] = str_3;
+
+    t_stock_str *res = ft_strs_to_tab(3, strs);
+    ft_show_tab(res);
+    free(res);
+    return (0);
+}
